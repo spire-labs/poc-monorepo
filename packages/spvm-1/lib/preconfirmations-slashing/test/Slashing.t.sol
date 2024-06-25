6907e139fe817b03e7e222adad3c2da24058059a
fluffypill0w <77161947+fluffypill0w@users.noreply.github.com>
2024-06-25 11:03:56 +0200

    Added contracts to lib folder

---
+++ packages/spvm-1/lib/preconfirmations-slashing/test/Slashing.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "election-contract/ElectionContract.sol";
+import "../src/Slashing.sol";
+
+contract SlashingTest is Test {
+    function testConstruction() public {
+        // not implemented
+        revert("not implemented");
+    }
+
+    function testSlash() public {
+        // not implemented
+        revert("not implemented");
+    }
+}
