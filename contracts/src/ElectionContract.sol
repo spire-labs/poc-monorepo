be283ffd9ff9949b30365c107e7d8ce593a7c1bb
yaonam <elimviolinist@gmail.com>
2024-06-26 08:03:25 -0700

    copied over contracts

---
+++ contracts/src/ElectionContract.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./ElectionInterface.sol";
+import "./GenericTicket.sol";
+
+contract ElectionContract is ElectionInterface {
+    address public minter;
+    address public default_recipient;
+    address public ticket_address;
+
+    // block number to winner mapping, used for historical blocks
+    mapping(uint256 => address) private winners;
+
+    uint256 public next_refresh_block;
+
+    constructor(address _minter) {
+        minter = _minter;
+
+        // deploy the ERC721 contract for proposal tickets
+        ticket_address = address(new GenericTicket(address(this)));
+
+        next_refresh_block = block.number;
+
+        // set the default recipient
+        default_recipient = msg.sender;
+    }
+
+    /// @notice Sets the default recipient for tickets.
+    function setDefaultRecipient(address _default_recipient) external {
+        require(
+            msg.sender == minter,
+            "Only the minter can set the default recipient"
+        );
+        default_recipient = _default_recipient;
+    }
+
+    /// @notice Returns the address of the winner of the election for a certain L1 block number.
+    function getWinner(
+        uint block_number
+    ) public view override returns (address) {
+        GenericTicket ticket = GenericTicket(ticket_address);
+
+        if (block_number >= next_refresh_block) {
+            return ticket.ownerOf(getWinnerTokenId(block_number));
+        } else {
+            return winners[block_number];
+        }
+    }
+
+    /// @dev Returns the tokenId of the winner of the election for a certain L1 block number. Used internally.
+    function getWinnerTokenId(
+        uint block_number
+    ) internal view returns (uint256) {
+        require(block_number <= block.number, "Cannot query future blocks");
+        require(
+            block_number > (block.number < 292 ? 0 : block.number - 292),
+            "Cannot query blocks older than 292 blocks"
+        );
+
+        GenericTicket ticket = GenericTicket(ticket_address);
+
+        // get historical supply of tickets at the given block number
+        uint historical_supply = ticket.historical_supply_at(block_number);
+
+        // get the historical block hash at the seed block for the given block number
+        uint historical_block_hash = uint(
+            blockhash(block_number < 64 ? 0 : block_number - 64)
+        );
+
+        uint winner_token_id = historical_block_hash % historical_supply;
+
+        return winner_token_id;
+    }
+
+    /// @notice Mints a ticket for the given address.
+    function mintTicket(address to) external {
+        require(msg.sender == minter, "Only the minter can mint new tickets");
+
+        GenericTicket(ticket_address).safeMint(to);
+    }
+
+    /// @notice Refreshes tickets and redistributes previous tickets to the default recipient.
+    /// This is needed so that a proposer that proposed a previous block cannot reuse a ticket.
+    /// Does not apply for the current block, but only for historical blocks.
+    function refreshTickets() external {
+        require(
+            block.number > next_refresh_block,
+            "Cannot refresh tickets again before the next block"
+        );
+
+        // check all tickets since last_block_updated
+        for (uint i = next_refresh_block; i < block.number; i++) {
+            uint256 tokenId = getWinnerTokenId(i);
+            winners[i] = GenericTicket(ticket_address).ownerOf(tokenId);
+            GenericTicket(ticket_address).useTicket(default_recipient, tokenId);
+        }
+
+        next_refresh_block = block.number; // update the next refresh block
+    }
+}
---
+++ contracts/src/GenericTicket.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GenericTicket is ERC721, ERC721Enumerable, Ownable {
+    uint256 private _nextTokenId;
+
+    mapping(uint => uint) public historical_supply;
+    uint last_block_updated;
+
+    constructor(
+        address initialOwner
+    ) ERC721("Ticket", "T") Ownable(initialOwner) {
+        _nextTokenId = 0;
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+    }
+
+    function useTicket(address to, uint256 tokenId) public onlyOwner {
+        // for testing purposes
+        _transfer(ownerOf(tokenId), to, tokenId);
+    }
+
+    function updateSupply() internal {
+        if (block.number - last_block_updated > 1) {
+            // update all blocks since last_block_updated
+            for (uint i = last_block_updated + 1; i < block.number; i++) {
+                historical_supply[i] = historical_supply[last_block_updated];
+            }
+        }
+
+        // update current block
+        historical_supply[block.number] = totalSupply();
+        last_block_updated = block.number;
+    }
+
+    function historical_supply_at(
+        uint block_number
+    ) public view returns (uint) {
+        require(block_number <= block.number, "Cannot query future blocks");
+        if (block_number <= last_block_updated) {
+            return historical_supply[block_number];
+        } else {
+            return totalSupply();
+        }
+    }
+
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal override(ERC721, ERC721Enumerable) returns (address) {
+        address out = super._update(to, tokenId, auth);
+
+        updateSupply();
+
+        return out;
+    }
+
+    function _increaseBalance(
+        address account,
+        uint128 value
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._increaseBalance(account, value);
+
+        updateSupply();
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
